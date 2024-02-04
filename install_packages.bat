ccd0bfaa4573fe0a598de178c925d4ef900a1470
Vaidik K. 3 <107250289+VaidikKhurana@users.noreply.github.com>
2024-02-04 13:28:25 +0530

    Add files via upload

---
+++ install_packages.bat
@@ -0,0 +1,45 @@
+@echo off
+REM Install pyttsx3
+pip install pyttsx3
+
+REM Install SpeechRecognition
+pip install SpeechRecognition
+
+REM Install colorama
+pip install colorama
+
+REM Install pygame
+pip install pygame
+
+REM Install requests
+pip install requests
+
+REM Install beautifulsoup4
+pip install beautifulsoup4
+
+REM Install pyautogui
+pip install pyautogui
+
+REM Install wikipedia
+pip install wikipedia
+
+REM Install selenium
+pip install selenium
+
+REM Install webdriver-manager
+pip install webdriver-manager
+
+REM Install pyserial
+pip install pyserial
+
+REM Install flask
+pip install Flask
+
+REM Install opencv-python
+pip install opencv-python
+
+REM Install pyaudio
+pip install pyaudio
+
+echo All packages installed successfully.
+pause
