3b0894c7cd3e4205e44cb5c9c052ec886bb8462c
Vaidik K. 3 <107250289+VaidikKhurana@users.noreply.github.com>
2024-02-04 13:32:04 +0530

    Add files via upload

---
+++ blackcape.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Please check if you have installed all the packages by running the install_packages file.
+timeout /t 3 >nul
+echo Running Login.py...
+python Login.py
+timeout /t 3 >nul
